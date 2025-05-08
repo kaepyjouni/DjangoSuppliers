808773798b62877bf8a6955dce25932c59e795e7
Joni <joni.a.reijo@gmail.com>
2025-05-08 11:12:46 +0300

    listaukset ja lisäykset

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import landingview, productlistview, supplierlistview
+from .views import landingview, productlistview, supplierlistview, addsupplier, addproduct
 
 urlpatterns = [
     path('', landingview),
 
     # Products url's
     path('products/', productlistview),
+    path('add-product/', addproduct),
 
     # Supplier url's
     path('suppliers/', supplierlistview),
+    path('add-supplier/', addsupplier),
 ]
